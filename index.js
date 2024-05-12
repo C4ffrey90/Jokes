0977d36e94d5de0022cb3974ccd0a0c98bd620d3
Wojciech Winiarski <w.winiarski09@gmail.com>
2024-05-12 14:41:39 +0200

    Initial commit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
+// API link: https://sv443.net/jokeapi/v2/
+// Available joke categories: Any, Misc, Programming, Dark, Pun, Spooky, Christmas
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
