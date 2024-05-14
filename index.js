1c32e16f18a89d409188dcde79f88bd1191ec9ff
Wojciech Winiarski <w.winiarski09@gmail.com>
2024-05-14 19:53:27 +0200

    Slightly changed template

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 import express from "express";
 import axios from "axios";
-
+import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
 // API link: https://sv443.net/jokeapi/v2/
 // Available joke categories: Any, Misc, Programming, Dark, Pun, Spooky, Christmas
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
 	res.render("index.ejs");
 });
 
+app.post("/get-joke", (req, res) => {
+	console.log(req.body);
+	res.render("index.ejs");
+});
 app.listen(port, () => {
 	console.log(`Listening to server on port ${port}`);
 });
